2924463d7fc47933c315ab0a7e88087291d296f8
esa0624 <117685119+esa0624@users.noreply.github.com>
2024-10-28 17:49:49 -0400

    Delete Composite directory

--- Composite/Composite_service/Composite/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('menu/<str:restaurant>/', views.get_menu, name='get_menu'),
-    path('order/', views.place_order, name='place_order'),
-    path('history/<str:user_id>/', views.get_user_order_history, name='get_user_order_history'),
-]
