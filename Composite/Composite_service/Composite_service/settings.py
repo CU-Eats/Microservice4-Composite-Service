906fcfab7683f73e6c8102b1e196d109ab3e87f7
dshen07 <dshen@ucsd.edu>
2024-12-10 15:13:26 -0500

    cleaning

--- Composite/Composite_service/Composite_service/settings.py
+++ Composite/Composite_service/Composite_service/settings.py
@@ -98,17 +98,6 @@ WSGI_APPLICATION = "Composite_service.wsgi.application"
 #         'PORT': '3306',
 #     }
 # }
-import os
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 
