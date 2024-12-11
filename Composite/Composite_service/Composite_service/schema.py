276661830f5ece4094d4f6164995b12253d55bfe
esa0624 <e_chen@coloradocollege.edu>
2024-12-11 00:10:34 -0500

    Update schema.py

--- Composite/Composite_service/Composite_service/schema.py
+++ Composite/Composite_service/Composite_service/schema.py
@@ -7,19 +7,19 @@ class RestaurantType(graphene.ObjectType):
     name = graphene.String()
     menu = graphene.List(graphene.String)
 
-class OrderType(graphene.ObjectType):
-    order_id = graphene.String()
-    status = graphene.String()
+# class OrderType(graphene.ObjectType):
+#     order_id = graphene.String()
+#     status = graphene.String()
 
 class Query(graphene.ObjectType):
     restaurant_details = graphene.Field(
         RestaurantType,
         restaurant_name=graphene.String(required=True)
     )
-    order_status = graphene.Field(
-        OrderType,
-        order_id=graphene.String(required=True)
-    )
+    # order_status = graphene.Field(
+    #     OrderType,
+    #     order_id=graphene.String(required=True)
+    # )
 
     def resolve_restaurant_details(self, info, restaurant_name):
         try:
@@ -33,16 +33,5 @@ class Query(graphene.ObjectType):
         except requests.exceptions.RequestException:
             return None
 
-    def resolve_order_status(self, info, order_id):
-        try:
-            endpoint = f"{settings.RESTAURANT_URL}/api/orders/{order_id}/status/"
-            response = requests.get(endpoint, timeout=10)
-            if response.status_code == 200:
-                status = response.json().get("status")
-                return OrderType(order_id=order_id, status=status)
-            else:
-                return None
-        except requests.exceptions.RequestException:
-            return None
 
 schema = graphene.Schema(query=Query)
